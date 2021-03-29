0b71c66e6a60ccf3113fe3ba996694687ceffbc9
f5074 <oseongryu@gmail.com>
2021-03-29 13:17:22 +0900

    [ResourceManager] Get Resource

---
+++ F5074.UI/View/H_Resource/Resource01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Resources;
+using System.Reflection;
+
+namespace F5074.UI.View.H_Resource
+{
+    public partial class Resource01 : UserControl
+    {
+        CultureInfo culture;
+
+        public Resource01()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            culture = CultureInfo.CreateSpecificCulture("en-US");
+            ResourceManager rm = new ResourceManager("F5074.UI.View.H_Resource.ResourceFile", Assembly.GetExecutingAssembly());
+            String strWebsite = rm.GetString("Website", culture);
+            String strName = rm.GetString("Name", culture);
+            Console.WriteLine("Website: " + strWebsite + "--Name: " + strName);
+        }
+
+    }
+}
